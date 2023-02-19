d86606d2041c8df4e0c0349ad1fd83ad9b3531ae
Usama <usamaamjid7@gmail.com>
2023-02-19 19:17:27 +0500

    added main file

---
+++ main.py
@@ -0,0 +1,12 @@
+from addition import Sum
+from subtraction import subtract
+from multiply import mul
+
+
+
+if __name__=="__main__":
+    A=6
+    B=4
+    print(Sum(A,B))
+    print(subtract(A,B))
+    print(mul(A,B))
